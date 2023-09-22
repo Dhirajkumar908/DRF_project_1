da8474e197b665c0cc0c0471812843ae7503f924
Dhiraj Kumar <kumardhirajkumar908@gmail.com>
2023-09-22 18:19:06 +0530

    Added edit/update function in post content

--- DRF_app/urls.py
+++ DRF_app/urls.py
@@ -5,7 +5,8 @@ urlpatterns=[
     path('', views.home, name='home'),
     path('post/', views.getallpost, name='getallpost'),
     path('createPost', views.createPost, name='createPost'),
-    path('delete', views.delete, name='delete')
-
+    path('delete', views.delete, name='delete'),
+    path('get_post', views.get_post,name='get_post'),
+    path('updatePost', views.updatePost, name='updatePost')
 
 ]
--- DRF_app/views.py
+++ DRF_app/views.py
@@ -9,13 +9,13 @@ from .serializers import postSerializer
 @api_view(['GET'])
 def home(request):
     return Response({"success": "set is successful"})
-
+#displaying the post 
 @api_view(['GET'])
 def getallpost(request):
     posts=post.objects.all()
     serializer=postSerializer(posts, many=True)
     return Response(serializer.data)
-
+#creating the post
 @api_view(['GET', 'POST'])
 def createPost(request):
     data=request.data
@@ -26,7 +26,7 @@ def createPost(request):
     else:
         return Response(serializer.errors, status=400)
     
-
+#deliting the post 
 @api_view(['DELETE'])
 def delete(request):
     post_id=request.data.get('post_id')
@@ -34,6 +34,38 @@ def delete(request):
         posts=post.objects.get(id=post_id)
         posts.delete()
         return Response({"success":"The post was successfull deleted"}, status=200)
-    except posts.DoesNotExist:
+    except post.DoesNotExist:
         return Response({"Error":"Post dose not exist"}, status=404)
+    
+#displaying one post    
+@api_view(['GET'])
+def get_post(request):
+    pk=request.data.get('id')
+    try:
+        posts=post.objects.get(id=pk)
+        serializer=postSerializer(posts)
+        print(serializer)
+        return Response(serializer.data)
+    except post.DoesNotExist:
+        return Response({"Error":"Post dose not exist"}, status=404)
+    
+#update the post
+@api_view(['PUT'])
+def updatePost(request):
+    post_id=request.data.get('post_id')
+    get_new_title=request.data.get('new_title')
+    get_new_content=request.data.get('new_content')
+    try:
+        posts=post.objects.get(id=post_id)
+        if get_new_title:
+            posts.title=get_new_title
+        if get_new_content:
+            posts.content=get_new_content
+        posts.save()
+        return Response({"success":"post updated successfuly"})
+    except post.DoesNotExist:
+        return Response({"upda":"Post dose updated"})
+
+
+    
 
